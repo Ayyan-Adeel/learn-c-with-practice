e455ed20bbc3ae37431fe037ca713b36fdcc7398
Dipsana Roy <dipsanaroy@gmail.com>
2025-04-20 09:44:39 +0000

    Updated Chapter 6: Example Programs
    Enhanced code snippets and internal comments.

--- Chapter_6/c6_call_by_value.c
+++ Chapter_6/c6_call_by_value.c
@@ -1,16 +1,17 @@
 // C6. Call by value function
-#include<stdio.h>
+#include <stdio.h>
 
 // Function Prototype
-int sum (int a, int b);
+int sum(int a, int b);
 
 int main()
 {
     int a = 4, b = 7;
+    printf("\na = %d and b = %d\n", a, b);
 
-    printf("\nThe value of a and b is %d and %d\n", a, b);
-    printf("The value of %d + %d is %d\n", a, b, sum (a, b)); // Function Call
-    printf("The value of a and b after function call is %d and %d\n", a, b);
+    printf("%d + %d is %d\n", a, b, sum(a, b)); // Function Call
+
+    printf("After function call, a = %d and b = %d\n", a, b);
 
     return 0;
 }
@@ -18,11 +19,11 @@ int main()
 // Function Description
 
 // Call by value
-int sum (int a, int b)
+int sum(int a, int b)
 {
     int c = a + b;
 
-    // The values of a and b will not change unless pointers are used
+    // a and b values will not change unless pointers are used
     a = 56465;
     b = 6546;
 
